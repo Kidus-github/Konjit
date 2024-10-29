1b92dcb822da69c8979ca33383d4d945a381ec6d
Kidus-github <kidus.muluwork@gmail.com>
2024-10-29 19:55:52 +0300

    added splash screen and edited some files

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initalization();
+  }
+
+  void initalization() async {
+    print('pausing....');
+    await Future.delayed(const Duration(seconds: 1));
+    print("play");
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
